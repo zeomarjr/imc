a758bf68cdce4e75a419c3a357ef8c348608bd97
zeomarjr <zeomar_jr@hotmail.com>
2022-10-14 10:49:09 -0300

    projeto IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  constructor(private renderer : Renderer2){}
 
-  constructor() {}
+  altura : string
+  peso: string
+  imc: string
+  saida: string
 
+  mudarTema(){
+    this.renderer.setAttribute(document.body, 'color-theme', 'dark')
+  }
+  
+trocaTema(event){
+  if(event.detail.checked){
+    this.renderer.setAttribute(document.body, 'color-theme', 'dark')
+  }else{
+    this.renderer.setAttribute(document.body, 'color-theme', 'light')
+  }
 }
+}
+
+
